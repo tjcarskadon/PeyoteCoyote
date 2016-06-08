36a85260eb9668d4fa7735cae2ec35b0773425b5
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-07 21:59:12 -0700

    FEAT - date picker refact for a mix of es6 and es5

--- roam/App/Components/Host.js
+++ roam/App/Components/Host.js
@@ -13,64 +13,65 @@ import {
   ActivityIndicatorIOS,
 } from 'react-native';
 
+
+//This is written in es5 because the date picker has problems with es6
 var Host = React.createClass( {
 
+   getDefaultProps () {
+      return {
+        date: new Date(),
+        timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
+      };
+    },
 
- getDefaultProps: function () {
-    return {
-      date: new Date(),
-      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
-    };
-  },
+    getInitialState () {
+      return {
+        date: this.props.date,
+        timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
+      };
+    },
 
-  getInitialState: function () {
-    return {
-      date: this.props.date,
-      timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
-    };
-  },
+    onDateChange (date) {
+      this.setState({date: date});
+    },
 
-  onDateChange: function(date) {
-    this.setState({date: date});
-  },
+    onTimezoneChange (event) {
+      var offset = parseInt(event.nativeEvent.text, 10);
+      if (isNaN(offset)) {
+        return;
+      }
+      this.setState({timeZoneOffsetInHours: offset});
+    },
 
-  onTimezoneChange: function (event) {
-    var offset = parseInt(event.nativeEvent.text, 10);
-    if (isNaN(offset)) {
-      return;
+    render () {
+      return (
+        <Image style={styles.backgroundImage} source={require('../../imgs/uni.jpg')}>
+        <TextInput
+            style={styles.submit} 
+            autoCapitalize="none"
+            placeholder="Enter Event Title"
+            placeholderTextColor="white"
+            // onChangeText={(text) => this.setState({email: text})} ###do something with this
+            // value={this.state.email} 
+            />
+            <DatePickerIOS
+            date={this.state.date}
+            mode="date"
+            timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+            onDateChange={this.onDateChange}
+          />
+            <TextInput
+            style={styles.submit} 
+            autoCapitalize="none"
+            placeholder="Enter roam description"
+            placeholderTextColor="white"
+            // onChangeText={(text) => this.setState({email: text})} ###do something with this
+            // value={this.state.email} 
+            />
+        </Image>
+
+      );
     }
-    this.setState({timeZoneOffsetInHours: offset});
-  },
-
-  render () {
-    return (
-      <Image style={styles.backgroundImage} source={require('../../imgs/uni.jpg')}>
-      <TextInput
-          style={styles.submit} 
-          autoCapitalize="none"
-          placeholder="Enter Event Title"
-          placeholderTextColor="white"
-          // onChangeText={(text) => this.setState({email: text})} ###do something with this
-          // value={this.state.email} 
-          />
-          <DatePickerIOS
-          date={this.state.date}
-          mode="date"
-          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
-          onDateChange={this.onDateChange}
-        />
-          <TextInput
-          style={styles.submit} 
-          autoCapitalize="none"
-          placeholder="Enter roam description"
-          placeholderTextColor="white"
-          // onChangeText={(text) => this.setState({email: text})} ###do something with this
-          // value={this.state.email} 
-          />
-      </Image>
-
-    );
   }
-}
 );
 module.exports = Host;
