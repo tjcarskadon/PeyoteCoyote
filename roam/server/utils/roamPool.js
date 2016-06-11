04e55c1dcf56ce85250b07344811001a9360aea3
JonasMS <jonas.sota@gmail.com>
2016-06-10 19:25:14 -0700

    FEAT - Auto Create a Pool Roam

--- roam/server/utils/roamPool.js
+++ roam/server/utils/roamPool.js
@@ -2,6 +2,7 @@ var apoc = require('apoc');
 
 const startRoam = require('./startRoam');
 const joinRoam = require('./joinRoam');
+const autoCreate = require('./autoCreate');
 
 //connect user to roam or create specified roam
 module.exports = (userInput, res) => {
@@ -20,12 +21,24 @@ module.exports = (userInput, res) => {
   //TODO: necessary?
   //const venue = {} = userInput ?
 
+
   if (isHost) {
-    startRoam(
-      userInput,
-      { locName, address },
-      res
-    );
+
+    //create poolRoam if a location
+    //is not specified
+    if (!!locName) {
+      startRoam(
+        userInput,
+        { locName, address },
+        res
+      );
+    } else {
+      //auto create poolRoam
+      autoCreate(
+        userInput,
+        res
+      )
+    }
 
   } else {
     //join specified roam
